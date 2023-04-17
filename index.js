f85b5a422315dad5bfe73f8fb8eec6cd0e23f21c
nik20vik <nverma20021998@gmail.com>
2023-04-18 02:17:14 +0530

    completed

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// MongoDB Connection
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db("fcc-urlshortener-microservice");
+const urls = db.collection("urls");
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,10 +22,41 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// requiring packages
+const dns = require('dns');
+const urlParser = require('url');
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+    // console.log(req.body);
+    const url = req.body.url;
+    const dnslookup = dns.lookup(urlParser.parse(url).hostname, async (err, address) => {
+        if(!address) {
+            res.json({error: "invalid url"});
+        } else {
+            const urlCount = await urls.countDocuments({});
+            const urlDoc = {
+                url,
+                short_url: urlCount
+            }
+
+            const result = await urls.insertOne(urlDoc);
+            // console.log(result);
+            res.json({ original_url: url, short_url: urlCount });
+        }
+    })
 });
+
+app.get('/api/shorturl/:short_url', async(req, res) => {
+    const short_url = req.params.short_url;
+    const urlDoc = await urls.findOne({ short_url: +short_url });
+    res.redirect(urlDoc.url);
+});
+
+// For /api/shorturl/<short_url>
+app.post('/api/shorturl/:short_url', function (req, res) {
+    
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
